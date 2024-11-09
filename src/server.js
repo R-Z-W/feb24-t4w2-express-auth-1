1b762624138e5e3e9dac06185032d109fa7c0413
R-Z-W <whiteleyzenki@gmail.com>
2024-11-09 12:20:59 +1100

    add ip to cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { User } = require("./models/UserModel");
 const { generateJWT, validateUserAuth } = require("./functions/jwtFunctions");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +9,12 @@ app.use(express.json());
 // Enable this if you want front-ends to have more freedom about how they make requests
 // eg. this is for HTML URL-encoded forms 
 // app.use(express.urlencoded({extended: true}));
+
+let corsOptions = {
+	origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", https://deployedreactapp.com"],
+	optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.get("/", (request, response) => {
 	response.json({
